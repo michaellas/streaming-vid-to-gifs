7fd587275fcc470df5a1161743ef9c7607287439
Scthe <marcin1113C@gmail.com>
2015-04-07 20:58:10 +0200

    Connect input with filters behaviour

--- src/input.py
+++ src/input.py
@@ -21,17 +21,35 @@ service_controller.declare_connection("videoInput", OutputMessageConnector(servi
 
 print 'starting input script'
 
+def __update_params_for_host(host_str, new_params):
+    import socket
+    import json
+    print "send to: %s msg: changed !" % host_str
+    
+    host, port = host_str.split(':')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, int(port)))
+    s.sendall(json.dumps(new_params) + '\n')
+    s.close()
+
 def update_all(root, cam, filters):
     read_successful, frame = cam.read() #odczyt obrazu z kamery
     new_filters = set()
-    if check1.get()==1: #sprawdzenie czy checkbox był zaznaczony
-        new_filters.add(1)
-
-    if filters ^ new_filters:
-        filters.clear()
-        filters.update(new_filters)
-        #zmiana wartości parametru "filtersOn" w zależności od checkbox'a
-        service_controller.update_params({"filtersOn": list(filters)})
+    
+    for i in range(3):
+        checked_now = checks[i].get() == 1
+        checked_previously = i in filters
+        # print "prev: %s, now: %s" % (checked_previously, checked_now)
+        host = hosts[i]
+        if( checked_previously != checked_now):
+            # checked change
+            print "changed: %d" % i
+            to_send = checked_now and [1] or []
+            __update_params_for_host( host, {"filtersOn": to_send })
+        if checked_now:
+            new_filters.add(i)
+    filters.clear()
+    filters.update(new_filters)
 
     if read_successful:
         frame_dump = frame.dumps() #zrzut ramki wideo do postaci ciągu bajtów
@@ -61,6 +79,9 @@ checkbox2.pack()
 check3=tk.IntVar()
 checkbox3 = tk.Checkbutton(root, text="Resize", variable=check3)
 checkbox3.pack()
+
+checks = [check1,check2,check3]
+hosts = ["localhost:11111","localhost:11112","localhost:11113"]
 
 #dołączenie metody update_all do głównej pętli programu, wynika ze specyfiki TKinter
 root.after(0, func=lambda: update_all(root, cam, set())) 
