b4854d224cf178ecff86037cb5989a29f8cb06a3
Scthe <marcin1113C@gmail.com>
2015-04-26 13:52:16 +0200

    Move script to class

--- src/gif_convert_service/GifConverter.py
+++ src/gif_convert_service/GifConverter.py
@@ -71,50 +71,73 @@ ffmpeg -r 16 -i out.webm -g 52 -acodec libvorbis -ab 64k -vcodec libvpx -vb 448k
 from subprocess import call
 import os
 
-FFMPEG_DIR = 'C:\\Users\\Marcin\\Desktop\\ffmpeg-20150215-git-2a72b16-win64-static\\bin'
-os.chdir(FFMPEG_DIR)
-FFMPEG_BIN = 'ffmpeg'
-OUT_FILE = 'out_cross.avi'
-len1,len2 = 55,20
-fade_start, fade_len = 50,15 # will end 10s after first video guaranteeing 33& alpha on clip end
-# fade_start, fade_len = 45,10
-w,h = 800,448
 
-# extract first frame
-cmd = [FFMPEG_BIN,
-	'-y', # allow override
-	'-i', 'eugene.avi',
-	'-t', '1', # only 1 second
-	'-r', '1', # one frame per second
-	'first_frame.png'
-	]
-ret_code = call(cmd, shell=True) # should be 0
+class GifConverter:
+	def __init__(self, ffmpeg_bin, out_dir):
+		self.ffmpeg_bin = ffmpeg_bin
+		self.out_dir = out_dir
 
-filter_ = ';'.join([
-	'color=white,fade=in:%d:%d[alpha]' % (fade_start, fade_len), # (start,duration)
-	'[alpha]scale=%d:%d[alpha1]' % (w,h),
-	'[1:v]scale=w=%d:h=%d:sws_flags=bicubic[over]' % (w,h),
-	'[over][alpha1]alphamerge[am]', # TODO apply to repeated first frame
+	def __call__(self, file_path):#, crossfade_len, crossfade_alpha):
+		len1,len2 = 55,20
+		fade_start, fade_len = 50,15 # will end 10s after first video guaranteeing 33& alpha on clip end
+		# fade_start, fade_len = 45,10
+		w,h = 800,448
+		
+		# extract first frame
+		first_frame_path = self.__create_first_frame_image(file_path)
+		self.__cross_fade(file_path, first_frame_path, fade_start, fade_len,w,h)
 
-	'[0:v][am]overlay=x=0:y=0:repeatlast=0' # 2nd over first
-	# '[0:v][over]overlay=50:0'
-	# '[0:v][alpha1]overlay=50:0'
-	])
-cmd = [FFMPEG_BIN,
-	'-y', # allow override
-	'-i', 'eugene.avi',
-	# '-i', 'eugene.avi',
-	# '-loop', '1','-i', 'img.jpg',
-	'-loop', '1','-i', 'first_frame.png',
-	# '-i', 'SSPP-gif.avi',
-	'-filter_complex', '"'+filter_+'"',
-	#'-r', '20',  # frames per second
-	'-preset', 'slow',
-	'-pix_fmt', 'rgb8',
-	'-b:v', '2M',
-	OUT_FILE
-	]
-cmd = ' '.join(cmd)
-print( cmd)
-print('*'*15)
-ret_code = call(cmd, shell=True) # should be 0
+	def __create_first_frame_image(self, video_path):
+		frame_path = 'first_frame.png'
+		cmd = [self.ffmpeg_bin,
+			'-y', # allow override
+			'-i', video_path,
+			'-t', '1', # only 1 second
+			'-r', '1', # one frame per second
+			frame_path
+			]
+		ret_code = call(cmd, shell=True) # should be 0
+		return frame_path
+
+	def __cross_fade(self, video_path, first_frame_path, fade_start, fade_len, w, h):
+		OUT_FILE = 'out_cross.avi'
+		filter_ = ';'.join([
+			'color=white,fade=in:%d:%d[alpha]' % (fade_start, fade_len), # (start,duration)
+			'[alpha]scale=%d:%d[alpha1]' % (w,h),
+			'[1:v]scale=w=%d:h=%d:sws_flags=bicubic[over]' % (w,h),
+			'[over][alpha1]alphamerge[am]', # TODO apply to repeated first frame
+
+			'[0:v][am]overlay=x=0:y=0:repeatlast=0' # 2nd over first
+			# '[0:v][over]overlay=50:0'
+			# '[0:v][alpha1]overlay=50:0'
+			])
+		cmd = [self.ffmpeg_bin,
+			'-y', # allow override
+			'-i', video_path,
+			# '-i', 'eugene.avi',
+			# '-loop', '1','-i', 'img.jpg',
+			'-loop', '1','-i', first_frame_path,
+			# '-i', 'SSPP-gif.avi',
+			'-filter_complex', '"'+filter_+'"',
+			#'-r', '20',  # frames per second
+			'-preset', 'slow',
+			'-pix_fmt', 'rgb8',
+			'-b:v', '2M',
+			OUT_FILE
+			]
+		cmd = ' '.join(cmd)
+		print( cmd)
+		print('*'*15)
+		ret_code = call(cmd, shell=True) # should be 0
+
+def read_cmd_args():
+	FFMPEG_DIR = 'C:\\Users\\Marcin\\Desktop\\ffmpeg-20150215-git-2a72b16-win64-static\\bin'
+	# os.chdir(FFMPEG_DIR)
+	FFMPEG_BIN = 'ffmpeg'
+	return os.path.join(FFMPEG_DIR,FFMPEG_BIN), 'out', 'out', os.path.join(FFMPEG_DIR,'eugene.avi')
+
+
+if __name__ == '__main__':
+	ffmpeg_bin, tmp_dir, out_dir, file_path = read_cmd_args()
+	script = GifConverter(ffmpeg_bin, out_dir)
+	script(file_path)
